a1d48802babc793f6056de43e5e6a06cb5c767e2
ishikawa <isikawa_m@outlook.jp>
2025-05-25 23:43:25 +0900

    appをつくる

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>mogitate</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+@extends('layouts.app')
 
+@section('css')
   <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-</head>
+@endsection
 
-<!-- http://localhost/productsでブラウザ表示する -->
-
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="index.php">
-        mogitate
-      </a>
-    </div>
-  </header>
+@section('content')
   <main>
     <div class="product__item">
       <h1>商品一覧</h1>
@@ -92,5 +77,4 @@
     </div>
     </div>
   </main>
-</body>
-</html>
+  @endsection
